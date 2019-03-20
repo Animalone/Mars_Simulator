247bb494fdf88803574ec15425da34a47d3c3caf
development <issacshen2015@gmail.com>
2019-03-19 17:39:43 -0700

    complete commandHandlerTest

--- test/simulator/SimpleCommandHandlerTest.java
+++ test/simulator/SimpleCommandHandlerTest.java
@@ -2,22 +2,128 @@ package simulator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import commandHandler.SimpleCommandHandler;
+import explorer.MarsExplorer;
 
 class SimpleCommandHandlerTest {
-	//test if CommandHandler can place the explorer, and if report can return proper message
+	//test if CommandHandler can place the explorer, and if report can return proper message (example a)
 	@Test
-	void testPlaceXY() {
-		HashMap<String, List<Integer>> dictionary = new HashMap<String, List<Integer>>();
+	void testPlace() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
 		dictionary.put("PLACE", List.of(0, 0));
 		dictionary.put("REPORT", null);
-		SimpleCommandHandler classUnderTest = new SimpleCommandHandler();
-		assertEquals("P:(0,0)", classUnderTest.process(dictionary));
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("P: (0,0)\n", classUnderTest.process(dictionary));
 	}
+	
+	//test if CommandHandler can place the explorer, and print the path after move command (example b)
+	@Test
+	void testMove() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("PLACE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(0, 1));
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("M: (0,0) (0,1)\n", classUnderTest.process(dictionary));
+	}
+	//test if CommandHandler can place the explorer, and print the path after move command (example c)
+	@Test
+	void testPlaceMoveReport() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("PLACE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(1, 1));
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("M: (0,0) (0,1) (1,1)\nP: (1,1)\n", classUnderTest.process(dictionary));
+	}
+	//test if MOVE COMMAND will cause the explorer fall
+	@Test
+	void testFall() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("PLACE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(6, 6));
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("P: (0,0)\n", classUnderTest.process(dictionary));
+	}
+	
+	//test if MOVE COMMAND does not make any move
+	@Test
+	void testNoMOVE() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("PLACE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(0, 0));
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("P: (0,0)\n", classUnderTest.process(dictionary));
+	}
+	//test if explorer is not on tabletop when it receive REPORT command
+	@Test
+	void testNoPlaceReport() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("", classUnderTest.process(dictionary));
+	}
+	
+	//test if explorer is not on tabletop when it receive MOVE command
+	@Test
+	void testNoPlaceMove() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("MOVE", List.of(0, 0));
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("", classUnderTest.process(dictionary));
+	}
+	
+	//test if explorer is not on tabletop when it receive MOVE and REPORT command
+	@Test
+	void testNoPlaceMoveReport() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("MOVE", List.of(0, 0));
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("", classUnderTest.process(dictionary));
+	}
+	//test if explorer can make right movement if it received valid MOVE command after non-valid MOVE command
+	@Test
+	void testValidMoveAfterNonValidMove() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("PLACE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(-1, -1));
+		dictionary.put("MOVE", List.of(1, 2));
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("M: (0,0) (0,1) (0,2) (1,2)\nP: (1,2)\n", classUnderTest.process(dictionary));
+	}
+	
+	//test if explorer can handle first PLACE command after a few Non PLACE command
+	@Test
+	void testValidFirstPlaceAfterNonPlace() {
+		LinkedHashMap<String, List<Integer>> dictionary = new LinkedHashMap<String, List<Integer>>();
+		dictionary.put("MOVE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(-1, -1));
+		dictionary.put("MOVE", List.of(1, 2));
+		dictionary.put("PLACE", List.of(0, 0));
+		dictionary.put("MOVE", List.of(1, 2));
+		dictionary.put("REPORT", null);
+		MarsExplorer explorer = new MarsExplorer();
+		SimpleCommandHandler classUnderTest = new SimpleCommandHandler(explorer);
+		assertEquals("M: (0,0) (0,1) (0,2) (1,2)\nP: (1,2)\n", classUnderTest.process(dictionary));
+	}
+	
 
 }
